2e06472573f3b2d50ac8709c91a2d2b4bfc08f57
Eshwar <eshwarvengala30@gmail.com>
2025-02-18 15:10:58 +0530

    working on about us and faqs

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AboutUs from './pages/AboutUs'
-
+import Faqs from './components/Faqs'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <AboutUs/>
-    </>
+    <div className="app">
+      {/* <AboutUs/>
+       */}
+       <Faqs/>
+    </div>
   )
 }
 
